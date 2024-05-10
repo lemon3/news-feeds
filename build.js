b998b0b95850ce495b8883a6a8d715df97b2d630
wolfgang <wolfgang@lemon3.at>
2024-05-10 18:25:15 +0200

    update

--- build.js
+++ build.js
@@ -1,15 +1,21 @@
 import { create, getCategories } from './tools/tools.js';
 import austria from './src/austria.js';
+import germany from './src/germany.js';
 
-const categories = getCategories(austria);
-categories.forEach((category) => {
+// AUSTRIA
+const categoriesAT = getCategories(austria);
+categoriesAT.forEach((category) => {
   create(
     austria.filter((val) => val.category.indexOf(category) > -1),
     `austria-${category}`
   );
 });
 
-// const sports = austria.filter(
-//   (val, ind) => val.category.indexOf('sports') > -1
-// );
-// create(sports, 'austria-sports');
+// GERMANY
+const categoriesDE = getCategories(germany);
+categoriesDE.forEach((category) => {
+  create(
+    germany.filter((val) => val.category.indexOf(category) > -1),
+    `germany-${category}`
+  );
+});
